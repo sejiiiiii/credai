a320853ad38f4459fcd28d3cf6b1167dcb76d9eb
Arjun Saji <arjunsajith52@gmail.com>
2026-01-18 12:45:30 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+class main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        System.out.println(numbers);
+    }
+}
